454df61ccc765b0880da3b66e8e0ca046cb8c21b
clemenzi <nc@vcz.me>
2026-08-15 23:36:46 +0200

    fix: service installation and startup handling

--- install.sh
+++ install.sh
@@ -6,13 +6,22 @@ echo "Installing Macaron..."
 
 SOURCE_DIR="/tmp/macaron-source-$(date +%s)"
 
+TARGET_USER="${SUDO_USER:-$USER}"
+if [ -n "${SUDO_USER:-}" ]; then
+  TARGET_HOME=$(dscl . -read "/Users/$TARGET_USER" NFSHomeDirectory | awk '{print $2}')
+else
+  TARGET_HOME="$HOME"
+fi
+
 git clone --depth 1 https://github.com/clemenzi/macaron.git "$SOURCE_DIR"
 cd "$SOURCE_DIR"
 
 cp macaron /usr/local/bin/
 chmod +x /usr/local/bin/macaron
 
-mkdir "$HOME/.config/macaron"
-mkdir "$HOME/.config/macaron/services"
+mkdir -p "$TARGET_HOME/.config/macaron/services"
+if [ -n "${SUDO_USER:-}" ]; then
+  chown -R "$TARGET_USER" "$TARGET_HOME/.config/macaron"
+fi
 
 echo "Macaron installed successfully!"
